a6aae2a6c2087ee55399caf1153dd7776575474b
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2024-12-29 13:52:50 +0530

    fix: add req,res in param in get current user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -206,7 +206,7 @@ const changeCurrentPassword = asyncHandler(async () => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
-const getCurrentUser = asyncHandler(async () => {
+const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, req.user, "current user fetched successfully"));
