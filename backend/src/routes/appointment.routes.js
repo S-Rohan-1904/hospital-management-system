d806c16dc96cf44348c4815d89bbb4f167397230
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2024-12-31 13:53:47 +0530

    fix: modify appointment routes

--- backend/src/routes/appointment.routes.js
+++ backend/src/routes/appointment.routes.js
@@ -9,10 +9,12 @@ import {
 import { verifyJWT } from "../middlewares/auth.middleware";
 const router = Router();
 
-router.route("/request-appointment").post(verifyJWT, requestAppointment);
-router.route("/approve-appointment").patch(verifyJWT, approveAppointment);
-router.route("/reject-appointment").patch(verifyJWT, rejectAppointment);
-router.route("/reschedule-appointment").patch(verifyJWT, rescheduleAppointment);
+router.route("/appointment").post(verifyJWT, requestAppointment);
+router.route("/appointment/:id/approve").patch(verifyJWT, approveAppointment);
+router.route("/appointment/:id/reject").patch(verifyJWT, rejectAppointment);
+router
+  .route("/appointment/:id/reschedule")
+  .patch(verifyJWT, rescheduleAppointment);
 router.route("/appointments").get(verifyJWT, getAppointments);
 
 export default router;
