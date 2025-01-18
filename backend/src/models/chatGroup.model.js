9c1dd1e21f123234f17976952fa8ecc6039157df
Dhayanidhi2005 <dhayanidhipurushothaman@gmail.com>
2025-01-18 14:24:26 +0530

    feat: add group chat and message model

---
+++ backend/src/models/chatGroup.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const chatGroupSchema = new mongoose.Schema({
+    groupName: {
+        type: String,
+        required: false
+    },
+    avatar: {
+        type: String,
+        required: false
+    },
+    groupchat: {
+        type: Boolean,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export const ChatGroup = mongoose.model('ChatGroup', chatGroupSchema);
---
+++ backend/src/models/chatMessage.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const chatMessageSchema = new Schema({
+    chatGroup: {
+        type: Schema.Types.ObjectId,
+        ref: 'ChatGroup',
+        required: true
+    },
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+},{
+    timestamps: true
+});
+
+export const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
