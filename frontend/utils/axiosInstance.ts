5cb3e648e9a64357cf35d82fed7db7d97bafea42
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2025-01-20 20:29:19 +0530

    fix: backend url

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL:
+    "https://hospital-management-system-backend-lewn.onrender.com/api/v1",
   withCredentials: true, // Include cookies by default
 });
 
