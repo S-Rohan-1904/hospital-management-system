0d712fdf2ee8da99b7b1f5a446b17b040378e9d4
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2025-01-20 21:02:25 +0530

    feat: add nearby hospital locator

---
+++ frontend/app/dashboard/nearby-hospital-locator/page.tsx
@@ -0,0 +1,11 @@
+import MapComponent from "./nearby-hospital-locator";
+
+const NearbyHospitalLocator = () => {
+    return (
+        <div>
+            <MapComponent />
+        </div>
+    );
+};
+
+export default NearbyHospitalLocator
